2ce183fa1d73c6765aa290eec1243cfad31ec02f
Zachary Williams <zacharywilliams080802@gmail.com>
2025-04-29 16:00:42 -0400

    grid experiment

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import viteLogo from '/vite.svg'
 function App() {
   return (
     <>
-      <div class="grid h-screen justify-center items-center">
-        <p class="text-5xl">Hi, I'm Zach!</p>
+      <div class="grid h-screen grid-cols-2 gap-4">
+        <p class="text-5xl content-center">Hi, I'm Zach!</p>
         <p class="text-xl">I'm a computer science and mathematics student from West Michigan. When I'm not programming, I'm playing the piano, running a D&D campaign, or teaching children how to fight each other with Tae Kwon Do.</p>
       </div>
     </>
