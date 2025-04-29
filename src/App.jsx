42730f88e83d9f6822777edb0337b7c5815876a1
Zachary Williams <zacharywilliams080802@gmail.com>
2025-04-29 16:09:16 -0400

    make ps flex

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import viteLogo from '/vite.svg'
 function App() {
   return (
     <>
-      <div class="grid h-screen grid-cols-2 gap-4 place-content-center">
-        <p class="text-5xl content-center">Hi, I'm Zach!</p>
-        <p class="text-xl content-center">I'm a computer science and mathematics student from West Michigan. When I'm not programming, I'm playing the piano, running a D&D campaign, or teaching children how to fight each other with Tae Kwon Do.</p>
+      <div class="grid h-screen grid-cols-2 gap-4">
+        <p class="flex text-5xl content-center">Hi, I'm Zach!</p>
+        <p class="flex text-xl content-center">I'm a computer science and mathematics student from West Michigan. When I'm not programming, I'm playing the piano, running a D&D campaign, or teaching children how to fight each other with Tae Kwon Do.</p>
       </div>
     </>
   )
