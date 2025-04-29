77185d7b1a65fcd7effba3eab32cf9db95d77358
Zachary Williams <zacharywilliams080802@gmail.com>
2025-04-29 15:53:55 -0400

    added blurb

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ function App() {
   return (
     <>
       <div class="flex h-screen justify-center items-center">
-        <p class="text-50xl">Hi, I'm Zach!</p>
+        <p class="text-5xl">Hi, I'm Zach!</p>
+        <p class="text-xl">I'm a computer science and mathematics student from West Michigan. When I'm not programming, I'm playing the piano, running a D&D campaign, or teaching children how to fight each other.</p>
       </div>
     </>
   )
