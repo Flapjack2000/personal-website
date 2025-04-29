038a9c643f633ed38cc74aa3cce071175b2599a2
Zachary Williams <zacharywilliams080802@gmail.com>
2025-04-29 16:05:49 -0400

    pull text to center

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import viteLogo from '/vite.svg'
 function App() {
   return (
     <>
-      <div class="grid h-screen grid-cols-2 gap-4">
+      <div class="grid h-screen grid-cols-2 gap-4 place-content-center">
         <p class="text-5xl content-center">Hi, I'm Zach!</p>
         <p class="text-xl content-center">I'm a computer science and mathematics student from West Michigan. When I'm not programming, I'm playing the piano, running a D&D campaign, or teaching children how to fight each other with Tae Kwon Do.</p>
       </div>
