d04fd8e705e402e48bb6bc85e1f703d94f42610b
Zachary Williams <zacharywilliams080802@gmail.com>
2025-04-29 15:57:17 -0400

    remove br tag

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ function App() {
     <>
       <div class="grid h-screen justify-center items-center">
         <p class="text-5xl">Hi, I'm Zach!</p>
-        <br />
         <p class="text-xl">I'm a computer science and mathematics student from West Michigan. When I'm not programming, I'm playing the piano, running a D&D campaign, or teaching children how to fight each other with Tae Kwon Do.</p>
       </div>
     </>
