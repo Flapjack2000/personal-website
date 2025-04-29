a39bc4c5896ae94b0cf060f5dcb9f3683140dd18
Zachary Williams <zacharywilliams080802@gmail.com>
2025-04-29 15:39:50 -0400

    try fixing div height to center vert

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,8 @@ import viteLogo from '/vite.svg'
 function App() {
   return (
     <>
-      {/* <div class="flex h-screen justify-center items-center">
+      <div class="flex h-screen justify-center items-center">
         <p class="text-5xl flex-1">Hi, I'm Zach!</p>
-      </div> */}
-
-      <div style="height:auto" class="flex items-center justify-center">
-        <p>Text centered vertically and horizontally.</p>
       </div>
     </>
   )
