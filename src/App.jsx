2cb3575dcae7a39384f1f3740a71594d2bc69ca2
Zachary Williams <zacharywilliams080802@gmail.com>
2025-04-29 21:06:14 -0400

    fix border

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <div class="grid h-screen grid-cols-2 gap-4">
-        <p style={{ borderLeft: "2px grey solid", }} class="text-5xl content-center">Hi, I'm Zach!</p>
+        <p style={{ borderRight: "2px grey solid", }} class="text-5xl content-center">Hi, I'm Zach!</p>
         <p class="text-xl content-center">I'm a computer science and mathematics student from West Michigan. When I'm not programming, I'm playing the piano, running a D&D campaign, or teaching children how to fight each other with Tae Kwon Do.</p>
       </div>
     </>
