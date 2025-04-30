905f3964a484e9d2dcdc84761706adac828fc437
Zachary Williams <zacharywilliams080802@gmail.com>
2025-04-30 10:54:07 -0400

    gotta import them too duh

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css'
 import App from './App.jsx'
+import About from './About.jsx';
+import Projects from './Projects.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
