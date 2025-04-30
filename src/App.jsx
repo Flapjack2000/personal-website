d9c934144a6a531bfb9d9b338b1d4fadead717d9
Zachary Williams <zacharywilliams080802@gmail.com>
2025-04-30 09:55:56 -0400

    actually centered the divs

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,11 @@ import viteLogo from '/vite.svg'
 function App() {
   return (
     <>
-      <div className="grid h-screen">
-        <div className="text-5xl content-center">Hi, I'm Zach!</div>
-        <div className="text-xl content-center max-h-60">I'm a computer science and mathematics student from West Michigan. When I'm not programming, I'm playing the piano, running a D&D campaign, or teaching children how to fight each other with Tae Kwon Do.</div>
+      <br />
+      <div className="grid gap-4">
+
+        <div className="text-5xl content-center text-center justify-self-center">Hi, I'm Zach!</div>
+        <div className="text-xl content-center text-center max-w-180 justify-self-center">I'm a computer science and mathematics student from West Michigan. When I'm not programming, I'm playing the piano, running a D&D campaign, or teaching children how to fight each other with Tae Kwon Do.</div>
       </div>
     </>
   )
