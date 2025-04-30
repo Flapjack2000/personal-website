6affff5450d790594fda4911b038cc44a6047994
Zachary Williams <zacharywilliams080802@gmail.com>
2025-04-30 10:49:40 -0400

    added new component pages to routes

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,13 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-  <BrowserRouter>
-    <Routes>
-      <Route index element={<App />} />
-    </Routes>
-  </BrowserRouter>
-  //</StrictMode>,
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<App />} />
+        <Route path='about' element={<About />} />
+        <Route path='projects' element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
 )
